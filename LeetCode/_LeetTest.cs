3a45b345c85632f2d96cbb1f939fac26b3e52c7c
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-09-29 18:22:45 -0700

    Longest valid parentheses

--- LeetCode/_LeetTest.cs
+++ LeetCode/_LeetTest.cs
@@ -1,18 +1,13 @@
 
 using System;
 using System.IO;
-
+using System.Collections;
 
 public class LeetTest {
     public static void Main(string[] args)
     {   
-        ListNode l1 = new ListNode(1);
-        l1.next = new ListNode(2);
-        l1.next.next = new ListNode(3);
-        l1.next.next.next = new ListNode(4);
-        
-        var s = new Solution025();
-        var res = s.ReverseKGroup(l1,3);     
-    
+        int[] input = new int[]{1,2};
+        var s = new Solution031();
+        s.NextPermutation(input);         
     }
 }
