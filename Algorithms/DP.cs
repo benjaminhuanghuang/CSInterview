ba8116d44c640a443e226cdf5ff51cdacf4f7ee0
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-11-08 14:41:01 -0800

    DP sample

---
+++ Algorithms/DP.cs
@@ -0,0 +1,29 @@
+
+long long Fibonacci_Solution1(unsigned int n)
+{
+      int result[2] = {0, 1};
+      if(n < 2)
+            return result[n];
+
+      return Fibonacci_Solution1(n - 1) + Fibonacci_Solution1(n - 2);
+}
+
+long long Fibonacci_Solution2(unsigned n)
+{
+      int result[2] = {0, 1};
+      if(n < 2)
+            return result[n];
+
+      long long  fibNMinusTwo = 0;
+      long long  fibNMinusOne = 1;
+      long long  fibN = 0;
+      for(unsigned int i = 2; i <= n; ++ i)
+      {
+            fibN = fibNMinusOne + fibNMinusTwo;
+
+            fibNMinusTwo = fibNMinusOne;
+            fibNMinusOne = fibN;
+      }
+
+       return fibN;
+}
