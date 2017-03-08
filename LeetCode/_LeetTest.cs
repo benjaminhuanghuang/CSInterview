28916a9b7d8fde7abd500dbaaac3745847016f9d
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-07 16:28:57 -0800

    update

--- LeetCode/_LeetTest.cs
+++ LeetCode/_LeetTest.cs
@@ -7,12 +7,12 @@ using System.Collections.Generic;
 public class LeetTest {
     public static void Main(string[] args)
     {   
-        int[,] matrix = new int[,] { {1, 2,3}, {4,5, 6},{7,8,9} };
-        var s = new Solution498();
-        var array = s.FindDiagonalOrder(matrix);
-        foreach (var i in array)
-        {
-            Console.WriteLine(i);
-        }
+        char[,] matrix = {{'E', 'E', 'E', 'E', 'E'},
+                        {'E', 'E', 'M', 'E', 'E'},
+                        {'E', 'E', 'E', 'E', 'E'},
+                        {'E', 'E', 'E', 'E', 'E'}};
+        var s = new Solution529();
+        var b = s.UpdateBoard(matrix, new int[]{3,0});
+        
     }
 }
