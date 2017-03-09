d3232b428a6c28842cf8e0088b884adcb76f1ccb
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-09 14:59:43 -0800

    update

--- LeetCode/_LeetTest.cs
+++ LeetCode/_LeetTest.cs
@@ -13,9 +13,16 @@ public class LeetTest
         // var end = "cog";
         // var wordList = new List<string> { "hot", "dot", "dog", "lot", "log", "cog" };
 
-        var begin = "hot";
-        var end = "dog";
-        var wordList = new List<string> { "hot", "dog"};
+        var begin = "qa";
+        var end = "sq";
+        var wordList = new List<string> { "si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb"
+        , "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", 
+        "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", 
+        "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", 
+        "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", 
+        "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", 
+        "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye" };
+
 
         var res = s.FindLadders(begin, end, wordList);
         foreach (var l in res)
