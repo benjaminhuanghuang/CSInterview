e46ca1e2b1f243eba3083ccdfbad556b8ff59fc8
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-09 17:21:26 -0800

    update

--- LeetCode/164_MaxGap.cs
+++ LeetCode/164_MaxGap.cs
@@ -14,37 +14,53 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-public class Solution164 {
+public class Solution164
+{
     //http://magicsong.xyz/leetcode/leetcode%E4%B9%8Bmaximum-gap%E8%A7%A3%E9%A2%98%E6%80%9D%E8%B7%AF%EF%BC%88c%EF%BC%89/
     // if we can sort it, it will be really easy. but it takes O(nlgn). 
     // Solve it in linear time. we can think about linear time sort, bucket sort algorithm.
-    // put all int into 32 buckets according to its length. then sort each bucket to get the maximum gap.
-    public int MaximumGap(int[] nums) {
-        var res = 0;
-        var buckets = new List<int>[32];
-        for (int i = 0; i < 32; i++)
-        {
-            buckets[i] = new List<int>();
-        }
+    // put all int into 32 buckets according to its length. then sort each bucket to get the 
+    // maximum gap.
+    public int MaximumGap(int[] nums)
+    {
+        if (nums == null || nums.Length < 2)
+            return 0;
+
+        var length = nums.Length;
+        var minVal = nums.Min();
+        var maxVal = nums.Max();
+
+        var bucketSize = Math.Max(1, (maxVal - minVal - 1) / (length - 1) + 1);
+        var bucketNumber = (maxVal - minVal) / bucketSize + 1;
+        var buckets = new Tuple<int, int>[bucketNumber];
+        
+        int bucketIndex = 0;
         foreach (var i in nums)
         {
-            var len = i.ToString().Length - 1;
-            buckets[len].Add(i);
+            bucketIndex = (i - minVal) / bucketSize;
+            Tuple<int,int> bucket = buckets[bucketIndex];
+            if (bucket == null)
+                buckets[bucketIndex] = new Tuple<int, int>(i, i);  // min an max value in bucket
+            else
+                buckets[bucketIndex] = new Tuple<int, int>(Math.Min(bucket.Item1, i), Math.Max(bucket.Item2, i));  // min an max value in bucket
         }
-
-        var previous = 0;
-        for (int i = 0; i < buckets.Count(); i++)
+        var ans = 0;
+        bucketIndex = 0;
+        while (bucketIndex < bucketNumber)
         {
-            buckets[i].Sort();
-
-            for (int j = 0; j < buckets[i].Count; j++)
+            if (buckets[bucketIndex] == null)
             {
-                if(previous != 0) res = Math.Max(res, buckets[i][j] - previous);
-                previous = buckets[i][j];
+                bucketIndex += 1;
+                continue;
             }
+            int nextBucket = bucketIndex + 1;
+            while (nextBucket < bucketNumber && buckets[nextBucket] == null)
+                nextBucket += 1;
+            if (nextBucket < bucketNumber)
+                ans = Math.Max(ans, buckets[nextBucket].Item1- buckets[bucketIndex].Item2);
+            bucketIndex = nextBucket;
         }
-
-        return res;
+        return ans;
     }
 }
 
--- LeetCode/_LeetTest.cs
+++ LeetCode/_LeetTest.cs
@@ -8,27 +8,8 @@ public class LeetTest
 {
     public static void Main(string[] args)
     {
-        var s = new Solution126();
-        // var begin = "hit";
-        // var end = "cog";
-        // var wordList = new List<string> { "hot", "dot", "dog", "lot", "log", "cog" };
-
-        var begin = "qa";
-        var end = "sq";
-        var wordList = new List<string> { "si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb"
-        , "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", 
-        "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", 
-        "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", 
-        "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", 
-        "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", 
-        "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye" };
-
-
-        var res = s.FindLadders(begin, end, wordList);
-        foreach (var l in res)
-        {
-            Console.Write("ladder: ");
-            Console.WriteLine(String.Join(" ", l));
-        }
+        var s = new Solution164();
+        var res = s.MaximumGap(new int[]{1, 10000000});
+        
     }
 }
