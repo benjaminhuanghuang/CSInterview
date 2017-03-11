cd9b76510a7885577952acd8d340afebb601637b
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-10 17:19:39 -0800

    update

--- LeetCode/126_WordLadder_II.cs
+++ LeetCode/126_WordLadder_II.cs
@@ -79,6 +79,7 @@ public class Solution126
                 if (c != word[i])
                 {
                     var charArr = word.ToCharArray();
+                    charArr[i] = c;
                     string nextWord = new string(charArr);
                     if (wordSet.Contains(nextWord))
                     {
@@ -93,7 +94,6 @@ public class Solution126
                     }
                 }
             }
-                    
         }
         
         return nextWords;
@@ -114,13 +114,15 @@ var begin = "hot";
 //---- Time Limit Exceeded
         var begin = "qa";
         var end = "sq";
-        var wordList = new List<string> { "si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb"
-        , "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", 
-        "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", 
-        "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", 
-        "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", 
-        "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", 
-        "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye" };
+        var wordList = new List<string> { "si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye" };
+
+        var s = new Solution126();
+        var res = s.FindLadders(begin, end, wordList);
+        foreach (var l in res)
+        {
+            Console.Write("ladder: ");
+            Console.WriteLine(String.Join(" ", l));
+        }
         
         
         //---- Time Limit Exceeded
