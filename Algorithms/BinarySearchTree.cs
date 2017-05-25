ac7c80b508a5510dd480a04ab80c6535522f45b8
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-05-25 16:35:13 -0700

    udpate

---
+++ Algorithms/BinarySearchTree.cs
@@ -0,0 +1,61 @@
+namespace CSInterview.Algorithms
+{
+    public class Node
+    {
+        public Node Left;
+        public Node Right;
+        public int Data;
+
+        public Node(int data)
+        {
+            this.Data = data;
+        }
+
+        public void Insert(int value)
+        {
+            if (value <= Data)
+            {
+                if (Left == null)
+                {
+                    Left = new Node(value);
+                }
+                else
+                {
+                    Left.Insert(value);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = new Node(value);
+                }
+                else
+                {
+                    Right.Insert(value);
+                }
+            }
+        }
+
+        public bool Contains(int value)
+        {
+            if (value == Data)
+            {
+                return true;
+            }
+            else if (value < Data)
+            {
+                if (Left == null)
+                    return false;
+                return Left.Contains(value);
+            }
+            else
+            {
+                if (Right == null)
+                    return false;
+                return Right.Contains(value);
+            }
+        }
+
+    }
+}
