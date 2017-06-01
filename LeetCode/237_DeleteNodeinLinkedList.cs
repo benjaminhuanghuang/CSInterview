a0c461a7647928624a9104e819298789ac2f8a3e
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-05-31 18:00:06 -0700

    update

--- LeetCode/237_DeleteNodeinLinkedList.cs
+++ LeetCode/237_DeleteNodeinLinkedList.cs
@@ -5,6 +5,8 @@ Write a function to delete a node (except the tail) in a singly linked list, giv
 
 Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list 
 should become 1 -> 2 -> 4 after calling your function.
+
+注意！如果node是list中的最后一个node，此题无解
 */
 
 public class Solution237 {
