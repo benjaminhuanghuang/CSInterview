84d4026c6509836bc2b85492c11b6f633bd499ea
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-06-01 16:42:11 -0700

    tiny url

--- LeetCode/534_Design TinyURL.cs
+++ LeetCode/534_Design TinyURL.cs
@@ -33,4 +33,78 @@ result in a peak QPS at a moment's notice.
 How could you handle redundancy? i,e, if a server is down, how could you ensure the service is 
 still operational?
 
+
+https://segmentfault.com/a/1190000006140476
 */
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+public class URLService
+{
+    Dictionary<string, int> ltos;
+    Dictionary<int, string> stol;
+    static int COUNTER;
+    string elements;
+    URLService()
+    {
+        ltos = new Dictionary<string, int>();
+        stol = new Dictionary<int, string>();
+        COUNTER = 1;
+        elements = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    public string longToShort(string url)
+    {
+        string shorturl = base10ToBase62(COUNTER);
+        ltos.Add(url, COUNTER);
+        stol.Add(COUNTER, url);
+        COUNTER++;
+        return "http://tiny.url/" + shorturl;
+    }
+    public string shortToLong(string url)
+    {
+        url = url.Substring("http://tiny.url/".Length);
+        int n = base62ToBase10(url);
+        return stol[n];
+    }
+
+    public int base62ToBase10(string s)
+    {
+        int n = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            n = n * 62 + CharToInt(s[i]);
+        }
+        return n;
+
+    }
+    public int CharToInt(char c)
+    {
+        if (c >= '0' && c <= '9')
+            return c - '0';
+        if (c >= 'a' && c <= 'z')
+        {
+            return c - 'a' + 10;
+        }
+        if (c >= 'A' && c <= 'Z')
+        {
+            return c - 'A' + 36;
+        }
+        return -1;
+    }
+    public string base10ToBase62(int n)
+    {
+        StringBuilder sb = new StringBuilder();
+        while (n != 0)
+        {
+            sb.Insert(0, elements[n % 62]);
+            n /= 62;
+        }
+        while (sb.Length != 6)
+        {
+            sb.Insert(0, '0');
+        }
+        return sb.ToString();
+    }
+}
