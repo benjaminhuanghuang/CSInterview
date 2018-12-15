938975dd01b2292fb76c010556b5529b87acda0d
Benjamin Huang <bhuang@rms.com>
2018-12-14 18:40:19 -0800

    update

--- LeetCode/064_MinimumPathSum.cs
+++ LeetCode/064_MinimumPathSum.cs
@@ -11,28 +11,30 @@ using System;
 
 public class Solution064 {
     public int MinPathSum(int[,] grid) {
-        var dp = new int[grid.GetLength(0), grid.GetLength(1)];
+        int rows = grid.GetLength(0);
+        int cols = grid.GetLength(1);
+        var dp = new int[rows, cols];
 
         dp[0, 0] = grid[0,0];
-        for (int i = 1; i < grid.GetLength(0); i++)
+        for (int i = 1; i < rows; i++)
         {
             // set first col
             dp[i, 0] = dp[i - 1, 0] + grid[i, 0];
         }
-        for (int i = 1; i < grid.GetLength(1); i++)
+        for (int i = 1; i < cols; i++)
         {
             //set first row
             dp[0, i] = dp[0, i - 1] + grid[0, i];
         }
 
-        for (int i = 1; i < grid.GetLength(0); i++)
+        for (int i = 1; i < rows; i++)
         {
-            for (int j = 1; j < grid.GetLength(1); j++)
+            for (int j = 1; j < cols; j++)
             {
                 dp[i, j] = Math.Min(dp[i - 1, j] + grid[i,j], dp[i, j - 1] + grid[i,j]);
             }
         }
 
-        return dp[grid.GetLength(0) - 1, grid.GetLength(1) - 1];
+        return dp[rows - 1, cols - 1];
     }
 }
